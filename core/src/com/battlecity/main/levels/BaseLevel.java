0a4219da071a23bd8a5b2156b98fcc65bb7f1a1b
Bogdan Onischenko <dever4eg@gmail.com>
2021-09-25 19:55:15 +0300

    Move random generator to base level

--- core/src/com/battlecity/main/levels/BaseLevel.java
+++ core/src/com/battlecity/main/levels/BaseLevel.java
@@ -11,6 +11,7 @@ import com.battlecity.main.global.Presets;
 import com.battlecity.main.net.*;
 import com.battlecity.main.render.IRenderable;
 import java.util.*;
+import java.util.Random;
 
 
 
@@ -52,7 +53,53 @@ public abstract class BaseLevel implements IRenderable
 
     public static final ArrayList<LevelMod> userLevels = new ArrayList();
 
-    abstract String[][] getTileArray();
+    String[][] getTileArray()
+    {
+        String[][] tiles = new String[Presets.MAX_TILE_VERTICAL][Presets.MAX_TILE_HORIZONTAL];
+
+        Random rand = new Random(-1881547168);
+
+        for (int i = 0; i < Presets.MAX_TILE_VERTICAL; i++) {
+            for (int j = 0; j < Presets.MAX_TILE_HORIZONTAL; j++) {
+                Tile.TileType tileType = Tile.TileType.values()[rand.nextInt(Tile.TileType.values().length)];
+                Tile.RegionType regionType = Tile.RegionType.values()[rand.nextInt(Tile.RegionType.values().length)];
+
+                if (tileType.id.equals(Tile.TileType.EAGLE.id)) {
+                    tileType = Tile.TileType.BRICKS;
+                }
+                if (tileType.id.equals(Tile.TileType.EAGLE_WALL.id)) {
+                    tileType = Tile.TileType.BRICKS;
+                }
+
+
+                if ((i >= 10 && i <= 12) && (j >= 10 && j <= 12)) {
+                    tileType = Tile.TileType.EAGLE_WALL;
+                    regionType = Tile.RegionType.FULL;
+                }
+
+                if (i == 11 && j == 11) {
+                    tileType = Tile.TileType.EAGLE;
+                }
+
+                if ((i >= 10 && i <= 12) && (j >= 20 && j <= 22)) {
+                    tileType = Tile.TileType.EAGLE_WALL;
+                    regionType = Tile.RegionType.FULL;
+                }
+
+                if (i == 11 && j == 21) {
+                    tileType = Tile.TileType.EAGLE;
+                }
+
+                if (tileType.id.equals(Tile.TileType.WATER.id)) {
+                    regionType = Tile.RegionType.FULL;
+                }
+
+                tiles[i][j] = tileType.id + ":" + regionType.id;
+            }
+        }
+
+        return tiles;
+    }
 
     Tile eagle;
     private Mode mode = Mode.ARCADE;
--- core/src/com/battlecity/main/levels/Level01.java
+++ core/src/com/battlecity/main/levels/Level01.java
@@ -11,55 +11,6 @@ import java.util.Random;
  */
 public class Level01 extends BaseLevel
 {
-
-    @Override
-    public String[][] getTileArray() {
-        String[][] tiles = new String[Presets.MAX_TILE_VERTICAL][Presets.MAX_TILE_HORIZONTAL];
-
-        Random rand = new Random(-1881547168);
-
-        for (int i = 0; i < Presets.MAX_TILE_VERTICAL; i++) {
-            for (int j = 0; j < Presets.MAX_TILE_HORIZONTAL; j++) {
-                Tile.TileType tileType = Tile.TileType.values()[rand.nextInt(Tile.TileType.values().length)];
-                Tile.RegionType regionType = Tile.RegionType.values()[rand.nextInt(Tile.RegionType.values().length)];
-
-                if (tileType.id.equals(Tile.TileType.EAGLE.id)) {
-                    tileType = Tile.TileType.BRICKS;
-                }
-                if (tileType.id.equals(Tile.TileType.EAGLE_WALL.id)) {
-                    tileType = Tile.TileType.BRICKS;
-                }
-
-
-                if ((i >= 10 && i <= 12) && (j >= 10 && j <= 12)) {
-                    tileType = Tile.TileType.EAGLE_WALL;
-                    regionType = Tile.RegionType.FULL;
-                }
-
-                if (i == 11 && j == 11) {
-                    tileType = Tile.TileType.EAGLE;
-                }
-
-                if ((i >= 10 && i <= 12) && (j >= 20 && j <= 22)) {
-                    tileType = Tile.TileType.EAGLE_WALL;
-                    regionType = Tile.RegionType.FULL;
-                }
-
-                if (i == 11 && j == 21) {
-                    tileType = Tile.TileType.EAGLE;
-                }
-
-                if (tileType.id.equals(Tile.TileType.WATER.id)) {
-                    regionType = Tile.RegionType.FULL;
-                }
-
-                tiles[i][j] = tileType.id + ":" + regionType.id;
-            }
-        }
-
-        return tiles;
-    }
-
     @Override
     public int getLevelID() {
         return 1;
